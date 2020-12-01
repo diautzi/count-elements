082bffce3a0e991b0a092eb65cb44e28a46e2b10
Diana Utan <didiutzi@gmail.com>
2020-12-01 02:41:33 +0000

    Automatically backed up by Learn

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,13 @@
+require 'pry'
 def count_elements(array)
   # code goes here
+  new_hash = Hash.new(0)
+  
+  array.each do |item|
+    # binding.pry
+    new_hash[item] += 1 
+    new_hash
+  end
 end
  
+ count_elements(['tortoise', 'aye-aye', 'honey badger', 'aye-aye', 'tortoise', 'tortoise'])
